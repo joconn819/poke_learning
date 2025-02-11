10843d4b12c6837c87505c7b0274162a69d77018
Justin O'Connor <justin.oconnor@outlook.com>
2025-02-11 15:27:34 -0700

    Pokemon class

---
+++ monsters/pokemon_data.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+Stats = {
+    0: "HP",
+    1: "Atk",
+    2: "Def",
+    3: "SpA",
+    4: "SpD",
+    5: "Spe"
+}
+
+class Natures(Enum):
+    HARDY = "Hardy"
+    LONELY = "Lonely"
+    BRAVE = "Brave"
+    ADAMANT = "Adamant"
+    NAUGHTY = "Naughty"
+    BOLD = "Bold"
+    DOCILE = "Docile"
+    RELAXED = "Relaxed"
+    IMPISH = "Impish"
+    LAX = "Lax"
+    TIMID = "Timid"
+    HASTY = "Hasty"
+    SERIOUS = "Serious"
+    JOLLY = "Jolly"
+    NAIVE = "Naive"
+    MODEST = "Modest"
+    MILD = "Mild"
+    QUIET = "Quiet"
+    BASHFUL = "Bashful"
+    RASH = "Rash"
+    CALM = "Calm"
+    GENTLE = "Gentle"
+    SASSY = "Sassy"
+    CAREFUL = "Careful"
+    QUIRKY = "Quirky"
---
+++ test/test_monsters.py
@@ -0,0 +1,22 @@
+import pytest
+from ..monsters.pokemon_monster import Pokemon
+from ..monsters.pokemon_data import Natures
+
+def test_charizard():
+    charizard = Pokemon(name="charizard", gender="male", held_item="Charizardite X", ability="Blaze", nature=Natures.ADAMANT, evs=[252, 252, 0, 0, 4, 0], ivs=[31, 31, 31, 31, 31, 31], moves=["Flare Blitz", "Dragon Claw", "Heat Wave", "Earthquake"])
+    
+    assert charizard.name == "charizard"
+    assert charizard.gender == "male"
+    assert charizard.held_item == "charizardite x"
+    assert charizard.ability == "blaze"
+    assert charizard.nature == Natures.ADAMANT
+    assert charizard.evs == [252, 252, 0, 0, 4, 0]
+    assert charizard.ivs == [31, 31, 31, 31, 31, 31]
+    assert charizard.moves == ["flare-blitz", "dragon-claw", "heat-wave", "earthquake"]
+
+    assert charizard.check_valid_pokemon()
+
+    charizard.set_ability("Solar Power")
+
+    assert not charizard.check_valid_pokemon()
+  
