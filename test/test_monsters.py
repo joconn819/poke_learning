6f5b5684e02c28caeba2577e8cfcec58303aa746
Justin O'Connor <justin.oconnor@outlook.com>
2025-02-12 09:44:54 -0700

    "Random order of teams"

--- test/test_monsters.py
+++ test/test_monsters.py
@@ -17,19 +17,30 @@ def test_charizard():
 
     assert charizard.check_valid_pokemon()
 
-    charizard.set_ability("Solar Power")
-
-    assert not charizard.check_valid_pokemon()
-
 def test_all_charizard_team():
-        charizard = Pokemon(name="charizard", gender="male", held_item="Charizardite X", ability="Blaze", nature=Natures.ADAMANT, evs=[252, 252, 0, 0, 4, 0], ivs=[31, 31, 31, 31, 31, 31], moves=["Flare Blitz", "Dragon Claw", "Heat Wave", "Earthquake"])
-        poke_team = PokemonTeam()
-        poke_team.add_pokemon(charizard)
-        poke_team.add_pokemon(charizard)
-        poke_team.add_pokemon(charizard)
-        poke_team.add_pokemon(charizard)
-        poke_team.add_pokemon(charizard)
-        poke_team.add_pokemon(charizard)
+    charizard = Pokemon(name="charizard", gender="male", held_item="Charizardite X", ability="Blaze", nature=Natures.ADAMANT, evs=[252, 252, 0, 0, 4, 0], ivs=[31, 31, 31, 31, 31, 31], moves=["Flare Blitz", "Dragon Claw", "Heat Wave", "Earthquake"])
+    poke_team = PokemonTeam()
+    poke_team.add_pokemon(charizard)
+    poke_team.add_pokemon(charizard)
+    poke_team.add_pokemon(charizard)
+    poke_team.add_pokemon(charizard)
+    poke_team.add_pokemon(charizard)
+    poke_team.add_pokemon(charizard)
 
-        assert poke_team.check_valid_team()
+    assert poke_team.check_valid_team()
+
+def test_pokemon_from_string():
+    string = """Calyrex-Ice @ Clear Amulet \nAbility: As One (Glastrier)  \nTera Type: Fairy  \nEVs: 252 HP / 116 Atk / 4 Def / 20 SpD / 116 Spe  \nAdamant Nature  \n- Glacial Lance  \n- High Horsepower  \n- Protect  \n- Trick Room  """
+    
+    calyrex_ice = Pokemon()
+    calyrex_ice.set_from_string(string)
+
+    assert calyrex_ice.name == "calyrex-ice"
+    assert calyrex_ice.held_item == "clear amulet"
+    assert calyrex_ice.ability == "as one (glastrier)"
+    assert calyrex_ice.tera_type == "Fairy"
+    assert calyrex_ice.evs == [252, 116, 4, 0, 20, 116]
+    assert calyrex_ice.nature == Natures.ADAMANT
+    assert calyrex_ice.moves == ["glacial lance", "high horsepower", "protect", "trick room"]
+
   
